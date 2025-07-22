3b4583e07e96444550cc14e4bc640d6ae1851b7d
charlie4284 <yangsoo.yoon@canonical.com>
2025-07-22 12:02:40 +0000

    update settings for local application deployment

--- django-hello-world/django_hello_world/django_hello_world/settings.py
+++ django-hello-world/django_hello_world/django_hello_world/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = "django_hello_world.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRESQL_DB_NAME"),
-        "USER": os.environ.get("POSTGRESQL_DB_USERNAME"),
-        "PASSWORD": os.environ.get("POSTGRESQL_DB_PASSWORD"),
-        "HOST": os.environ.get("POSTGRESQL_DB_HOSTNAME"),
-        "PORT": os.environ.get("POSTGRESQL_DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("POSTGRESQL_DB_NAME"),
+#         "USER": os.environ.get("POSTGRESQL_DB_USERNAME"),
+#         "PASSWORD": os.environ.get("POSTGRESQL_DB_PASSWORD"),
+#         "HOST": os.environ.get("POSTGRESQL_DB_HOSTNAME"),
+#         "PORT": os.environ.get("POSTGRESQL_DB_PORT"),
+#     }
+# }
 
 
 # Password validation
